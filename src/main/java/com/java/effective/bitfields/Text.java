302f0df5b19f3fe75ec2659e1a59cd615fc9f62d
Oleg <oleg.bezkorovajniy@gmail.com>
2015-08-23 03:08:26 +0300

    Use EnumSet instead of bit fields, Never derive a value, associated with an enum from its ordinal;

---
+++ src/main/java/com/java/effective/bitfields/Text.java
@@ -0,0 +1,29 @@
+package com.java.effective.bitfields;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+/**
+ * Created by Oleg on 23 Aug 2015.
+ * EnumSet - a modern replacement for bit fields
+ */
+public class Text {
+
+    public enum Style {
+
+        BOLD, ITALIC, UNDERLINE, STRIKETHROUGH
+
+    }
+        // Any Set could be passed in, but EnumSet is clearly best
+        public void applyStyles(Set<Style> styles) {
+            // body goes here
+        }
+    public static void main(String[] args) {
+
+        Text text = new Text();
+
+        text.applyStyles(EnumSet.of(Style.ITALIC, Style.BOLD, Style.STRIKETHROUGH));
+
+    }
+
+}
---
+++ src/main/java/com/java/effective/instancefield/Ensemble.java
@@ -0,0 +1,22 @@
+package com.java.effective.instancefield;
+
+/**
+ * Created by Oleg on 23 Aug 2015.
+ * Never derive a value associated with an enum from its ordinal;
+ * store it in an instance field instead
+ */
+public enum Ensemble {
+    SOLO(1), DUET(2), TRIO(3), QUARTET(4), QUINTET(5), SIXTET(6),
+    SEPTET(7), OCTET(8), DOUBLE_QUARTET(8), NONET(9),
+    DECTET(10), TRIPLE_QUARTET(12);
+
+    private final int numberOfMusicians;
+
+    Ensemble(int size) {
+        this.numberOfMusicians = size;
+    }
+
+    public int getNumberOfMusicians() {
+        return numberOfMusicians;
+    }
+}
