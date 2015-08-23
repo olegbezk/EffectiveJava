245bbe32fc7a56f87690c97ca28b40914db28455
Oleg <oleg.bezkorovajniy@gmail.com>
2015-08-23 21:13:33 +0300

    add flexibility to API

---
+++ src/main/java/com/java/effective/generics/flexebility/Stack.java
@@ -0,0 +1,60 @@
+package com.java.effective.generics.flexebility;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.EmptyStackException;
+
+/**
+ * Created by Oleg on 23 Aug 2015.
+ *
+ */
+public class Stack <E> {
+
+    private E[] elements;
+
+    private int size = 0;
+
+    private int INITIAL_CAPACITY = 16;
+
+    @SuppressWarnings("unchecked")
+    public Stack() {
+        elements = (E[]) new Object[INITIAL_CAPACITY];
+    }
+
+    public void push(E e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+
+    public void pushAll(Iterable<? extends E> src) {
+        for (E e : src) {
+            push(e);
+        }
+    }
+
+    public E pop() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        E result = elements[--size];
+        elements[size] = null;
+        return result;
+    }
+
+    public void popAll(Collection<? super E> dst) {
+        while (!isEmpty()) {
+            dst.add(pop());
+        }
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void ensureCapacity() {
+        if (elements.length == size) {
+            elements = Arrays.copyOf(elements, 2 * size + 1);
+        }
+    }
+
+}
